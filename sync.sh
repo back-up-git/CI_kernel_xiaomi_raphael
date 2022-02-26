53fb3f051100dad3818ab18a610362e014f902a2
azure <azurehelper01@gmail.com>
2022-02-26 15:39:16 +0530

    Fix kernel tree sync error
    Signed-off-by: AzurE <azurehelper01@gmail.com>

--- sync.sh
+++ sync.sh
@@ -10,9 +10,8 @@ mkdir -p $CIRRUS_WORKING_DIR
 cd $CIRRUS_WORKING_DIR
 
 # Syncing source files
-echo "Syncing Kernel Tree"
-
 git clone $KT_LINK $KERNEL_ROOTDIR || { echo "ERROR: Failed to Clone the Kernel Tree!" && exit 1; }
+cd $KERNEL_ROOTDIR
 
 # Exit
 exit 0
