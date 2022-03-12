6bdc8a7cf7d91d340176eb12f3e46448f3c1dd04
AzurE-007 <100161001+AzurE-007@users.noreply.github.com>
2022-03-12 22:54:26 +0530

    Update vars.sh

--- vars.sh
+++ vars.sh
@@ -3,3 +3,10 @@
 export KT_LINK="https://github.com/AzurE-007/kernel_xiaomi_raphael.git -b android11"
 
 export KERNEL_ROOTDIR="$CIRRUS_WORKING_DIR/$DEVICE_CODENAME"
+
+
+export SYNC_PATH="$HOME/work" # Full (absolute) path. Use "$HOME" instead of "~".
+export USE_CCACHE=1
+export CCACHE_SIZE="50G"
+export CCACHE_DIR="$HOME/work/.ccache"
+export J_VAL=16
